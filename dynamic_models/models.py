ee5e07df4083c92a587cd958d401cb70b8b86eec
Ryan Vinzent <rvinzent217@hotmail.com>
2018-11-02 21:07:24 -0700

    Unchecked and uncached dynamic model constructor

--- dynamic_models/models.py
+++ dynamic_models/models.py
@@ -1,32 +1,45 @@
+"""
+Contains the base models and default implementations of dynamic model classes.
+The concrete default implementations can only be used when the 'dynamic_models'
+app is installed, but the base classes can be used for a custom implementation
+without installing the app.
+"""
 from django.db import models
-from django.conf import settings
-from model_utils import Choices, FieldTracker
+from model_utils import Choices
 
-from .utils import slugify_underscore
+from . import utils
 
 
 class BaseDynamicModel(models.Model):
     """
-    Base model for the dynamic model definition table.
+    Base model for the dynamic model definition table. The base model does not
+    guarantee unique table names. Table name uniqueness should be handled by the
+    user upon subclassing.
     """
     name = models.CharField(max_length=32)
-    fields = models.ManyToManyField(
+    _fields = models.ManyToManyField(
         'DynamicField',
-        through='DynamicModelFields'
+        through='DynamicModelField'
     )
 
     class Meta:
         abstract = True
 
-    def __str__(self):
-        return self.name
+    @property
+    def fields(self):
+        """
+        Returns the through table field instances instead of the dynamic field
+        instances directly .
+        """
+        return self._fields.through.objects.filter(model=self)
 
     @property
     def model_name(self):
         """
-        Default model name is the capitalized name of the instance.
+        Default model name is the capitalized name of the instance without
+        spaces.
         """
-        return self.name.capitalize()
+        return self.name.title().replace(' ', '')
 
     @property
     def table_name(self):
@@ -34,17 +47,56 @@ class BaseDynamicModel(models.Model):
         Default table name is the slugified instance name with underscores
         instead of hyphens.
         """
-        return slugify_underscore(self.name)
+        return utils.slugify_underscore(self.name)
 
     def get_dynamic_model(self):
-        pass
+        """
+        Dynamically defines the model class represented by this instance. If
+        regenerate is set to True, the cache will be ignored and the model will
+        be regenerated from scratch. If the model has not changed and
+        regenerate is set to False, the model will be retrieved from the cache.
+        """
+        return type(self.model_name, (models.Model,), self._model_attrs())
+
+    def _model_meta(self):
+        """
+        Returns a Meta class for constructing a Django model. The Meta class
+        sets the app_label, model_name, db_table, and verbose name.
+        """
+        class Meta:                          # pylint: disable=missing-docstring
+            app_label = self._meta.app_label
+            model_name = self.model_name
+            db_table = self.table_name
+            verbose_name = self.name
+        return Meta
+
+    def _model_fields(self):
+        """
+        Returns the model fields of the model being generated.
+        """
+        return {f.name: f.get_model_field() for f in self.fields}
+
+    def _model_attrs(self):
+        """
+        Returns a dict of the attributes to be used in creation of the dynamic
+        model class.
+        """
+        attrs = {'__module__': "{}.models".format(self._meta.app_label)}
+        attrs.update(
+            Meta=self._model_meta(),
+            **utils.default_fields(),
+            **self._model_fields()
+        )
+        return attrs
 
 
 class BaseDynamicField(models.Model):
     """
-    Base model for dynamic field definitions
+    Base model for dynamic field definitions. Data type choices are stored in
+    the DATA_TYPES class attribute. Each data type should have a key set in
+    FIELD_TYPES corresponding to the constructor to be called when generating
+    the model field.
     """
-
     DATA_TYPES = Choices(
         ('char', 'short text'),
         ('text', 'long text'),
@@ -73,73 +125,44 @@ class BaseDynamicField(models.Model):
     class Meta:
         abstract = True
 
-    def __str__(self):
-        return self.name
-
     @property
     def column_name(self):
         """
         Returns the name of the database column created by this field.
         """
-        return slugify_underscore(self.name)
+        return utils.slugify_underscore(self.name)
 
     def get_model_field(self, **options):
         """
         Returns a Django model field instance based on the instance's data type
         and name.
         """
-        return self._field_constructor(**options)
+        constructor = self._field_constructor(self.data_type)
+        return constructor(db_column=self.column_name, **options)
 
     @classmethod
     def _field_constructor(cls, data_type):
         return cls.FIELD_TYPES[data_type]
 
 
-class DynamicModel(BaseDynamicModel):
-    """
-    By default, a 'created_by' field is used on the dynamic field along with
-    a unique together constraint between the name and user. No two models should
-    have the same table name, so unique constraints must be applied to prevent
-    this from happenning.
-    """
-    created_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
-    )
-
-    class Meta:
-        unique_together = (('name', 'created_by'),)
-
-    @property
-    def table_name(self):
-        return slugify_underscore(f'{super().table_name}_{self.created_by}')
-
-
-class DynamicField(BaseDynamicField):
-    """
-    By default, a 'created_by' field is used on the dynamic field along with
-    a unique together constraint between the name and user. No two fields should
-    have the same name.
-    """
-    created_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
-    )
-
-    class Meta:
-        unique_together = (('name', 'created_by'),)
-
-
 class BaseDynamicModelField(models.Model):
     """
     Customizing the through table allows fields with the same name and data type
     to be declared with different options. The value of 'required' is sets
-    whether a field is nullable.
+    Django's 'null' and 'blank' options when declaring the field.
     """
-    model = models.ForeignKey(DynamicModel, on_delete=models.CASCADE)
-    field = models.ForeignKey(DynamicField, on_delete=models.CASCADE)
+    model = models.ForeignKey(
+        utils.dynamic_model_class_name(),
+        on_delete=models.CASCADE
+    )
+    field = models.ForeignKey(
+        utils.dynamic_field_class_name(),
+        on_delete=models.CASCADE
+    )
     required = models.BooleanField(default=False)
     unique = models.BooleanField(default=False)
+    max_length = models.PositiveIntegerField(null=True)
+    # TODO: indexable fields
 
     class Meta:
         abstract = True
@@ -154,9 +177,9 @@ class BaseDynamicModelField(models.Model):
             'blank': not self.required,
             'unique': self.unique
         }
-        if self.field.data_type == self.field.DATA_TYPES.char:
-            options['max_length'] = 128
-        return self.field.get_model_field(**options)
+        if self.max_length:
+            options['max_length'] = self.max_length
+        return self.field.get_model_field(**options) # pylint: disable=no-member
 
 
 class DynamicModelField(BaseDynamicModelField):
--- dynamic_models/utils.py
+++ dynamic_models/utils.py
@@ -1,4 +1,38 @@
+"""
+Various utility functions for the dynamic models app.
+"""
 from django.utils.text import slugify
+from django.conf import settings
+
 
 def slugify_underscore(text):
+    """
+    Returns the slugified text with hyphens replaced by underscores.
+    """
     return slugify(text).replace('-', '_')
+
+def dynamic_model_class_name():
+    """
+    Returns the name of the dynamic model class or DynamicModel if none is
+    provided by the user.
+    """
+    return settings.DYNAMIC_MODELS.get(
+        'DYNAMIC_MODEL_CLASS',
+        'dynamic_models.DynamicModel'
+    )
+
+def dynamic_field_class_name():
+    """
+    Returns the name of the dynamic field class or DynamicField if none is
+    provided by the user.
+    """
+    return settings.DYNAMIC_MODELS.get(
+        'DYNAMIC_FIELD_CLASS',
+        'dynamic_models.DynamicField'
+    )
+
+def default_fields():
+    """
+    Returns the DEFAULT_FIELDS setting.
+    """
+    return settings.DYNAMIC_MODELS.get('DEFAULT_FIELDS', {})
