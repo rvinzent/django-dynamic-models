095913e0c884a6f0d4809e94054338eec564b9ca
Vinzent, Ryan <ryan.vinzent@pioneer.com>
2018-11-20 11:01:59 -0800

    Moved settings module to project root, added dummy backend

---
+++ settings/base.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = 'supersecret'
+
+INSTALLED_APPS = [
+	'dynamic_models',
+	'django.contrib.contenttypes',
+]
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.dummy'
+	}
+}
+
+CACHES = {
+	'default': {
+		'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
+	}
+}
---
+++ settings/postgres.py
@@ -0,0 +1,17 @@
+"""
+PostgreSQL specific test settings.
+"""
+# pylint: disable=W0614, W0401
+from decouple import config
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DYNAMIC_MODELS_DB', default='dynamic_models'),
+        'USER': config('DYNAMIC_MODELS_DB_USER', default='dynamic_models'),
+        'PASSWORD': config('DYNAMIC_MODELS_DB_PASSWORD', default=''),
+        'HOST': config('DYNAMIC_MODELS_POSTGRES_HOST', default='127.0.0.1'),
+        'PORT': config('DYNAMIC_MODELS_POSTGRES_PORT', default='5432', cast=int)
+    }
+}
