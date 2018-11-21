6a26e8e671eb55acf004102aa44aa3dafa493631
Ryan Vinzent <rvinzent217@hotmail.com>
2018-11-20 20:27:22 -0800

    Added test app migration and placed it in INSTALLED_APPS

--- settings/base.py
+++ settings/base.py
@@ -8,18 +8,19 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'supersecret'
 
 INSTALLED_APPS = [
-	'dynamic_models',
-	'django.contrib.contenttypes',
+    'dynamic_models',
+    'tests',
+    'django.contrib.contenttypes',
 ]
 
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.dummy'
-	}
+    'default': {
+        'ENGINE': 'django.db.backends.dummy'
+    }
 }
 
 CACHES = {
-	'default': {
-		'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
-	}
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
+    }
 }
